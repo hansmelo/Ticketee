401beb712b8dd936793081cf3773ca12a942dca0
Hans Melo <hansmelo@gmail.com>
2016-07-16 21:47:07 -0300

    crud complete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
   def index
     @projects = Project.all
   end
@@ -11,24 +12,21 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      flash[:notice] = "Project has been created."
+      flash[:notice] = 'Project has been created.'
       redirect_to @project
     else
-      flash.now[:alert] = "Project has not been created."
-      render "new"
+      flash.now[:alert] = 'Project has not been created.'
+      render 'new'
     end
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = 'Project has been updated.'
       redirect_to @project
@@ -39,7 +37,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = 'Project has been deleted.'
     redirect_to projects_path
@@ -49,5 +46,12 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'The project you were looking for could not be found.'
+    redirect_to projects_path
   end
 end
